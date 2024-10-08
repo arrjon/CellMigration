2e3c98ef640600fb475725b7cae9366d0b0841ab
arrjon <jonas.arruda@uni-bonn.de>
2024-10-08 15:43:05 +0200

    updated plotting routines

---
+++ synth_data_params_bayesflow/load_data.py
@@ -0,0 +1,125 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from summary_stats import cut_region
+
+
+def load_real_data(data_id: int, max_sequence_length: int, cells_in_population: int,
+                   plot_data: bool = False, plot_starting_area: bool = False, plot_chemokine: bool = False):
+
+    if not data_id in [0, 1]:
+        raise ValueError("The data_id should be either 0 or 1")
+
+    # load real data
+    real_data_id = [0, 1][data_id]
+    if real_data_id == 0:
+        real_data_df = pd.read_csv('../real_data/37C-1_crop.csv')
+        y_size = 739.79  # microns
+        x_size = 279.74  # microns
+        x_offset, y_offset = -7.5, -2  # correction such that the pillars are in the middle
+        y_lin_shift = 0
+    else:
+        # more important data set, closer to the truth
+        real_data_df = pd.read_csv('../real_data/37C_ctrl2.csv')
+        y_size = 882.94  # microns
+        x_size = 287.03  # microns
+        x_offset, y_offset = -2, -7.5  # 3, 7.5  # correction such that the pillars are in the middle
+        y_lin_shift = -0.04  # 0.05  # correction for the tilt of the data
+
+    # define the window
+    # simulation coordinates, length of the gap: 1351-1145 = 206
+    # in real data: 270
+    # 270/206 = 1.31
+    # morpheus size of image: 1173x2500
+    factor = 1.31
+    # reconstruction of the window assuming that it is centered
+    # morpheus coordinates: 1173x2500x0
+    window_x1, window_x2 = (1173 - y_size / factor) / 2., 1173 - (1173 - y_size / factor) / 2.
+    window_y1, window_y2 = (2500 - x_size / factor) / 2., 2500 - (2500 - x_size / factor) / 2.
+
+    # remove first three rows
+    real_data_df = real_data_df.iloc[3:]
+    # only keep positions and time
+    real_data_df = real_data_df[['TRACK_ID', 'POSITION_X', 'POSITION_Y', 'POSITION_T']]
+    # convert to numeric
+    real_data_df = real_data_df.apply(pd.to_numeric, errors='coerce')
+
+    # scale to morpheus coordinates
+    real_data_scaled_df = real_data_df.copy()
+    real_data_scaled_df['x'] = real_data_scaled_df['POSITION_X'] / factor + window_x1
+    real_data_scaled_df['y'] = real_data_scaled_df['POSITION_Y'] / factor + window_y1
+    # real cells are moving downwards, but in simulations they are going upwards
+    real_data_scaled_df['y'] = 2500 - real_data_scaled_df['y']
+    # data does not fit optimally, so we need to shift it
+    real_data_scaled_df['x'] += x_offset
+    real_data_scaled_df['y'] += y_offset
+    # data is tilted
+    real_data_scaled_df['x'] = real_data_scaled_df['x'] + (window_y2 - real_data_scaled_df['y']) * y_lin_shift
+    # time
+    real_data_scaled_df['t'] = real_data_scaled_df['POSITION_T']
+
+    real_data = []
+    cut_region_real_data = True
+    sequence_lengths_real = []
+    # each cell is of different length, each with x and y coordinates, make a tensor out of it
+    for s_id, sample in enumerate(real_data_scaled_df.TRACK_ID.unique()):
+        cell = real_data_scaled_df[real_data_scaled_df.TRACK_ID == sample]
+        # order by time
+        cell = cell.sort_values('POSITION_T', ascending=True)
+        sequence_lengths_real.append(len(cell['y']))
+        if cut_region_real_data:
+            cell = cut_region(cell, x_min=316.5, x_max=856.5, y_min=1145, y_max=1351, return_longest=True)
+            if cell is None:
+                continue
+            cell = cell[0]
+        # pre-pad the data with zeros, but first write zeros as nans to compute the mean and std
+        track = np.ones((max_sequence_length + 1, 2)) * np.nan
+        track[-len(cell['x'][:max_sequence_length]):, 0] = cell['x'][:max_sequence_length]
+        track[-len(cell['y'][:max_sequence_length]):, 1] = cell['y'][:max_sequence_length]
+        real_data.append(track[1:])  # remove the first time point, same as in the simulation (often nan anyway)
+
+    real_data = np.stack(real_data)
+    real_data_full = real_data.copy()
+    real_data = real_data[:cells_in_population * (real_data_full.shape[0] // cells_in_population)]
+
+    # plot the real data
+    if plot_data:
+        fig = plt.figure(figsize=(10, 10))
+        plt.plot(real_data_full[0, :, 0], real_data_full[0, :, 1], 'r', label='Real Cell Trajectories')
+        for cell_id in range(1, real_data_full.shape[0]):
+            plt.plot(real_data_full[cell_id, :, 0], real_data_full[cell_id, :, 1], 'r')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.plot([415 / 1.31, 415 / 1.31], [1145, 1351], 'black',
+                 label='visible window (in simulations)')  # left vertical line
+        plt.plot([856.5, 856.5], [1145, 1351], 'black')  # right vertical line
+        plt.plot([415 / 1.31, 856.5], [1145, 1145], 'black')  # lower horizontal line
+        plt.plot([415 / 1.31, 856.5], [1351, 1351], 'black')  # upper horizontal line
+
+        # plot starting area
+        if plot_starting_area:
+            #starting_center = 1173./2, 1500./ factor / 2
+            starting_center = 1173. / 2, 2500. / 3
+            starting_radius = 65 / factor
+            circle = plt.Circle(starting_center, starting_radius, color='lightblue', fill=False, label='Random Starting Area')
+            ax = plt.gca()
+            ax.add_patch(circle)
+        #
+        # # add chemokine
+        if plot_chemokine:
+            starting_center = 1173./2, (1500+1500/2+270)/ factor
+            starting_radius = 550
+            circle = plt.Circle(starting_center, starting_radius, color='orange', alpha=0.5, fill=True, label='Chemokine (std)')
+            ax = plt.gca()
+            ax.add_patch(circle)
+
+        plt.plot([window_x1, window_x1], [window_y1, window_y2], color='grey', label='actual window')  # left vertical line
+        plt.plot([window_x2, window_x2], [window_y1, window_y2], color='grey')  # right vertical line
+        plt.plot([window_x1, window_x2], [window_y1, window_y1], color='grey')  # lower horizontal line
+        plt.plot([window_x1, window_x2], [window_y2, window_y2], color='grey')  # upper horizontal line
+        tiff_im = plt.imread('Cell_migration_grid_v3_final2_invers.tiff')
+        plt.imshow(tiff_im, origin='lower')
+        plt.legend()
+        plt.show()
+    return real_data, real_data_full
---
+++ synth_data_params_bayesflow/plotting_routines.py
@@ -0,0 +1,313 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+
+from summary_stats import reduced_coordinates_to_sumstat, compute_mean_summary_stats
+
+
+def plot_violin(ax, data, label, ylabel, alpha=0.05):
+    colors = ['#1f77b4', '#ff7f0e']  # Blue for non-significant, orange for significant
+    n_sim_plots = len(data) - 2
+
+    plot = ax.violinplot(data, showmedians=True, showextrema=False)
+    p_values = []
+    for i, pc in enumerate(plot['bodies'][1:], start=1):
+        a_test = stats.anderson_ksamp((data[0], data[i]), method=stats.PermutationMethod())
+        p_values.append(a_test.pvalue)
+        color = colors[1] if a_test.pvalue < alpha else colors[0]
+        pc.set_facecolor(color)
+        pc.set_edgecolor('black')
+        pc.set_alpha(0.8)
+
+    ax.set_xticks(np.arange(n_sim_plots + 2) + 1, ['Data', 'Median-Simulation'] + ['Simulation'] * n_sim_plots,
+                  rotation=90)
+    ax.set_ylabel(ylabel)
+    if np.sum(np.array(p_values) < alpha) > (len(data) - 1) // 2:
+        ax.set_title(f'{label}\n(Statistically Different)')
+    else:
+        ax.set_title(f'{label}\n')
+
+
+def plot_compare_summary_stats(test_sim: list, posterior_sim: list, path: str = None, seed: int = 42):
+    """
+    Compare the summary statistics of the test simulation and the posterior simulations using the Anderson-Darling
+    k-sample test and violin plots. Assumes that the first simulation of the posterior is the median/map.
+    """
+
+    if len(test_sim) != 1:
+        raise ValueError("The test simulation should only have one population")
+
+    np.random.seed(seed)
+    factor = 1.31  # to scale to real world coordinates, assumes that data is Morpheus coordinates
+
+    # compute the summary statistics
+    synthetic_summary_stats_list = [reduced_coordinates_to_sumstat(t * factor) for t in
+                                    test_sim]  # should be only one population
+    simulation_synth_summary_stats_list = [reduced_coordinates_to_sumstat(pop_sim) for pop_sim in posterior_sim]
+
+    # compute the mean of the summary statistics
+    (ad_mean_synth, ad_mean_synth_averg,
+     MSD_mean_synth, MSD_mean_synth_averg,
+     TA_mean_synth, TA_mean_synth_averg,
+     VEL_mean_synth, VEL_mean_synth_averg,
+     WT_mean_synth, WT_mean_synth_averg) = compute_mean_summary_stats(synthetic_summary_stats_list)
+    (ad_mean_synth_sim, ad_mean_synth_sim_averg,
+     MSD_mean_synth_sim, MSD_mean_synth_sim_averg,
+     TA_mean_synth_sim, TA_mean_synth_sim_averg,
+     VEL_mean_synth_sim, VEL_mean_synth_sim_averg,
+     WT_mean_synth_sim, WT_mean_synth_sim_averg) = compute_mean_summary_stats(simulation_synth_summary_stats_list)
+
+    fig, ax = plt.subplots(nrows=1, ncols=5, tight_layout=True, figsize=(12, 5))
+
+    # always include the median (index 0)
+    if len(ad_mean_synth_sim) == 1:
+        random_index = [0]
+    else:
+        random_index = np.random.choice(range(1, len(ad_mean_synth_sim)), min(5, len(ad_mean_synth_sim) - 1), replace=False)
+        random_index = np.append(0, random_index)
+
+    # Perform the Anderson-Darling k-sample test and plot for each statistic
+    # Angle Degree
+    plot_violin(ax[0], [ad_mean_synth[0]] + [ad_mean_synth_sim[i] for i in random_index],
+                'Angle Degree', 'Angle Degree (degrees)\nMean per Cell')
+
+    # Mean Squared Displacement (MSD)
+    plot_violin(ax[1], [MSD_mean_synth[0]] + [MSD_mean_synth_sim[i] for i in random_index],
+                'Mean Squared Displacement', 'MSD\nMean per Cell')
+
+    # Turning Angle
+    plot_violin(ax[2], [TA_mean_synth[0]] + [TA_mean_synth_sim[i] for i in random_index],
+                'Turning Angle', 'Turning Angle (radians)\nMean per Cell')
+
+    # Velocity
+    plot_violin(ax[3], [VEL_mean_synth[0]] + [VEL_mean_synth_sim[i] for i in random_index],
+                'Velocity', 'Velocity\nMean per Cell')
+
+    # Waiting Time
+    plot_violin(ax[4], [WT_mean_synth[0]] + [WT_mean_synth_sim[i] for i in random_index],
+                'Waiting Time', 'Waiting Time (sec)\nMean per Cell')
+
+    if path is not None:
+        plt.savefig(f'{path}')
+    plt.show()
+
+    # Wasserstein distance
+    wasserstein_distance = stats.wasserstein_distance(ad_mean_synth_sim[0], ad_mean_synth[0])
+    wasserstein_distance += stats.wasserstein_distance(MSD_mean_synth_sim[0], MSD_mean_synth[0])
+    wasserstein_distance += stats.wasserstein_distance(TA_mean_synth_sim[0], TA_mean_synth[0])
+    wasserstein_distance += stats.wasserstein_distance(VEL_mean_synth_sim[0], VEL_mean_synth[0])
+    wasserstein_distance += stats.wasserstein_distance(WT_mean_synth_sim[0], WT_mean_synth[0])
+    print(f"Wasserstein distance: {wasserstein_distance}")
+
+    return wasserstein_distance
+
+
+def plot_trajectory(test_sim: np.ndarray, posterior_sim: np.ndarray,
+                    path: str = None, label_true: str = None,
+                    two_plots: bool = False, show_image: bool = False):
+    """
+    Plot the trajectories of the test simulation and one posterior simulation.
+    """
+    cells_in_population = test_sim.shape[0]
+
+    if two_plots:
+        fig, ax = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True, tight_layout=True, figsize=(10, 5))
+    else:
+        fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True, tight_layout=True, figsize=(5, 5))
+        ax = [ax, ax]
+    tiff_im = plt.imread('Cell_migration_grid_v3_final2_invers.tiff')
+
+    # plot the simulations or the MAP
+    if show_image and two_plots:
+        ax[1].imshow(tiff_im)
+    ax[1].plot(posterior_sim[0, :, 0], posterior_sim[0, :, 1], 'b', label='Simulated Trajectories', alpha=1)
+    for cell_id in range(1, cells_in_population):
+        ax[1].plot(posterior_sim[cell_id, :, 0], posterior_sim[cell_id, :, 1], 'b')
+
+    # plot the synthetic data
+    if show_image:
+        ax[0].imshow(tiff_im)
+    if label_true is None:
+        label_true = 'Synthetic Trajectories'
+    ax[0].plot(test_sim[0, :, 0], test_sim[0, :, 1], 'r', label=label_true, alpha=1)
+    for cell_id in range(1, cells_in_population):
+        ax[0].plot(test_sim[cell_id, :, 0], test_sim[cell_id, :, 1], 'r')
+
+    ax[0].set_ylabel('y')
+    for a in ax:
+        a.legend()
+        a.set_xlabel('x')
+    if path is not None:
+        plt.savefig(f'{path}')
+    plt.show()
+
+
+    #
+    # fig, ax = plt.subplots(nrows=1, ncols=5, tight_layout=True, figsize=(12, 5))
+    # alpha = 0.05
+    # colors = ['#1f77b4', '#ff7f0e']  # Blue for non-significant, orange for significant
+    # np.random.seed(42)
+    # random_index = np.random.choice(range(len(ad_mean_synth_sim)), 5, replace=False)
+    #
+    # def plot_violin(ax, data, label, ylabel):
+    #     n_sim_plots = len(data) - 2
+    #
+    #     plot = ax.violinplot(data, showmedians=True, showextrema=False)
+    #     p_values = []
+    #     for i, pc in enumerate(plot['bodies'][1:], start=1):
+    #         a_test = stats.anderson_ksamp((data[0], data[i]), method=stats.PermutationMethod())
+    #         p_values.append(a_test.pvalue)
+    #         color = colors[1] if a_test.pvalue < alpha else colors[0]
+    #         pc.set_facecolor(color)
+    #         pc.set_edgecolor('black')
+    #         pc.set_alpha(0.8)
+    #
+    #     ax.set_xticks(np.arange(n_sim_plots + 2) + 1, ['Data', 'MAP-Simulation'] + ['Simulation'] * n_sim_plots,
+    #                   rotation=90)
+    #     ax.set_ylabel(ylabel)
+    #     if np.sum(np.array(p_values) < alpha) > (len(data) - 1) // 2:
+    #         ax.set_title(f'{label}\n(Statistically Different)')
+    #     else:
+    #         ax.set_title(f'{label}\n')
+    #
+    # # Perform the Anderson-Darling k-sample test and plot for each statistic
+    # # Angle Degree
+    # plot_violin(ax[0],
+    #             [ad_mean_synth[0], ad_mean_synth_sim[map_idx_sim]] + [ad_mean_synth_sim[i] for i in random_index],
+    #             'Angle Degree', 'Angle Degree (degrees)\nMean per Cell')
+    #
+    # # Mean Squared Displacement (MSD)
+    # plot_violin(ax[1],
+    #             [MSD_mean_synth[0], MSD_mean_synth_sim[map_idx_sim]] + [MSD_mean_synth_sim[i] for i in random_index],
+    #             'Mean Squared Displacement', 'MSD\nMean per Cell')
+    #
+    # # Turning Angle
+    # plot_violin(ax[2],
+    #             [TA_mean_synth[0], TA_mean_synth_sim[map_idx_sim]] + [TA_mean_synth_sim[i] for i in random_index],
+    #             'Turning Angle', 'Turning Angle (radians)\nMean per Cell')
+    #
+    # # Velocity
+    # plot_violin(ax[3],
+    #             [VEL_mean_synth[0], VEL_mean_synth_sim[map_idx_sim]] + [VEL_mean_synth_sim[i] for i in random_index],
+    #             'Velocity', 'Velocity\nMean per Cell')
+    #
+    # # Waiting Time
+    # plot_violin(ax[4],
+    #             [WT_mean_synth[0], WT_mean_synth_sim[map_idx_sim]] + [WT_mean_synth_sim[i] for i in random_index],
+    #             'Waiting Time', 'Waiting Time (sec)\nMean per Cell')
+    #
+    # plt.savefig(f'{checkpoint_path}/Summary Stats.png')
+    # plt.show()
+    #
+    # # Wasserstein distance
+    # wasserstein_distance = stats.wasserstein_distance(ad_mean_synth_sim[map_idx_sim], ad_mean_synth[0])
+    # wasserstein_distance += stats.wasserstein_distance(MSD_mean_synth_sim[map_idx_sim], MSD_mean_synth[0])
+    # wasserstein_distance += stats.wasserstein_distance(TA_mean_synth_sim[map_idx_sim], TA_mean_synth[0])
+    # wasserstein_distance += stats.wasserstein_distance(VEL_mean_synth_sim[map_idx_sim], VEL_mean_synth[0])
+    # wasserstein_distance += stats.wasserstein_distance(WT_mean_synth_sim[map_idx_sim], WT_mean_synth[0])
+    # print(f"Wasserstein distance: {wasserstein_distance}")
+    # # %% raw
+    # # plot the summary statistics
+    # fig, ax = plt.subplots(nrows=3, ncols=5, sharey='col', tight_layout=True, figsize=(12, 8))
+    # n_sim_plots = 3
+    #
+    # # Perform the Kolmogorov-Smirnov test
+    # # ks_statistic, p_value = stats.ks_2samp(ad_mean_synth_sim[map_idx_sim], ad_mean_synth[0])
+    # a_test = stats.anderson_ksamp((ad_mean_synth_sim[map_idx_sim], ad_mean_synth[0]), method=stats.PermutationMethod())
+    # ks_statistic, p_value = a_test.statistic, a_test.pvalue
+    # print(f"Angle Degree KS Statistic: {ks_statistic}")
+    # print(f"Angle Degree P-value: {p_value}, {p_value < 0.05}: different distributions")
+    # ax[0, 0].violinplot([ad_mean_synth_sim[map_idx_sim], ad_mean_synth[0]], showmeans=True)
+    # if p_value < 0.05:
+    #     ax[0, 0].set_title(f'Angle Degree\n(Different)')
+    # else:
+    #     ax[0, 0].set_title(f'Angle Degree\n(Same)')
+    # ax[0, 0].set_ylabel(f'Angle Degree (degrees)\nMean per Cell')
+    # ax[1, 0].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[1, 0].violinplot([ad_mean_synth_sim_averg, ad_mean_synth_averg], showmeans=True)
+    # ax[1, 0].set_ylabel(f'Angle Degree (degrees)\nPopulation Mean')
+    # ax[0, 0].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[2, 0].violinplot([ad_mean_synth_sim[map_idx_sim]] + [ad_mean_synth_sim[i] for i in range(n_sim_plots)],
+    #                     showmeans=True)
+    # ax[2, 0].set_xticks(np.arange(n_sim_plots + 1) + 1, ['Map'] + ['Simulation'] * n_sim_plots, rotation=60)
+    #
+    # # ks_statistic, p_value = stats.ks_2samp(MSD_mean_synth_sim[map_idx_sim], MSD_mean_synth[0])
+    # a_test = stats.anderson_ksamp((MSD_mean_synth_sim[map_idx_sim], MSD_mean_synth[0]),
+    #                               method=stats.PermutationMethod())
+    # ks_statistic, p_value = a_test.statistic, a_test.pvalue
+    # print(f"MSD KS Statistic: {ks_statistic}")
+    # print(f"MSD P-value: {p_value}, {p_value < 0.05}: different distributions")
+    # ax[0, 1].violinplot([MSD_mean_synth_sim[map_idx_sim], MSD_mean_synth[0]], showmeans=True)
+    # if p_value < 0.05:
+    #     ax[0, 1].set_title(f'Mean Squared Displacement\n(Different)')
+    # else:
+    #     ax[0, 1].set_title(f'Mean Squared Displacement\n(Same)')
+    # ax[0, 1].set_ylabel(f'MSD\nMean per Cell')
+    # ax[0, 1].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[1, 1].violinplot([MSD_mean_synth_sim_averg, MSD_mean_synth_averg], showmeans=True)
+    # ax[1, 1].set_ylabel(f'MSD\nPopulation Mean')
+    # ax[1, 1].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[2, 1].violinplot([MSD_mean_synth_sim[map_idx_sim]] + [MSD_mean_synth_sim[i] for i in range(n_sim_plots)],
+    #                     showmeans=True)
+    # ax[2, 1].set_xticks(np.arange(n_sim_plots + 1) + 1, ['Map'] + ['Simulation'] * n_sim_plots, rotation=60)
+    #
+    # # ks_statistic, p_value = stats.ks_2samp(TA_mean_synth_sim[map_idx_sim], TA_mean_synth[0])
+    # a_test = stats.anderson_ksamp((TA_mean_synth_sim[map_idx_sim], TA_mean_synth[0]), method=stats.PermutationMethod())
+    # ks_statistic, p_value = a_test.statistic, a_test.pvalue
+    # print(f"Turning Angle KS Statistic: {ks_statistic}")
+    # print(f"Turning Angle P-value: {p_value}, {p_value < 0.05}: different distributions")
+    # ax[0, 2].violinplot([TA_mean_synth_sim[map_idx_sim], TA_mean_synth[0]], showmeans=True)
+    # if p_value < 0.05:
+    #     ax[0, 2].set_title(f'Turning Angle\n(Different)')
+    # else:
+    #     ax[0, 2].set_title(f'Turning Angle\n(Same)')
+    # ax[0, 2].set_ylabel(f'Turning Angle (radians)\nMean per Cell')
+    # ax[0, 2].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[1, 2].violinplot([TA_mean_synth_sim_averg, TA_mean_synth_averg], showmeans=True)
+    # ax[1, 2].set_ylabel(f'Turning Angle (radians)\nPopulation Mean')
+    # ax[1, 2].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[2, 2].violinplot([TA_mean_synth_sim[map_idx_sim]] + [TA_mean_synth_sim[i] for i in range(n_sim_plots)],
+    #                     showmeans=True)
+    # ax[2, 2].set_xticks(np.arange(n_sim_plots + 1) + 1, ['Map'] + ['Simulation'] * n_sim_plots, rotation=60)
+    #
+    # # ks_statistic, p_value = stats.ks_2samp(VEL_mean_synth_sim[map_idx_sim], VEL_mean_synth[0])
+    # a_test = stats.anderson_ksamp((VEL_mean_synth_sim[map_idx_sim], VEL_mean_synth[0]),
+    #                               method=stats.PermutationMethod())
+    # ks_statistic, p_value = a_test.statistic, a_test.pvalue
+    # print(f"Velocity KS Statistic: {ks_statistic}")
+    # print(f"Velocity P-value: {p_value}, {p_value < 0.05}: different distributions")
+    # ax[0, 3].violinplot([VEL_mean_synth_sim[map_idx_sim], VEL_mean_synth[0]], showmeans=True)
+    # if p_value < 0.05:
+    #     ax[0, 3].set_title(f'Velocity\n(Different)')
+    # else:
+    #     ax[0, 3].set_title(f'Velocity\n(Same)')
+    # ax[0, 3].set_ylabel(f'Velocity\nMean per Cell')
+    # ax[0, 3].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[1, 3].violinplot([VEL_mean_synth_sim_averg, VEL_mean_synth_averg], showmeans=True)
+    # ax[1, 3].set_ylabel(f'Velocity\nPopulation Mean')
+    # ax[1, 3].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[2, 3].violinplot([VEL_mean_synth_sim[map_idx_sim]] + [VEL_mean_synth_sim[i] for i in range(n_sim_plots)],
+    #                     showmeans=True)
+    # ax[2, 3].set_xticks(np.arange(n_sim_plots + 1) + 1, ['Map'] + ['Simulation'] * n_sim_plots, rotation=60)
+    #
+    # # ks_statistic, p_value = stats.ks_2samp(WT_mean_synth_sim[map_idx_sim], WT_mean_synth[0])
+    # a_test = stats.anderson_ksamp((WT_mean_synth_sim[map_idx_sim], WT_mean_synth[0]), method=stats.PermutationMethod())
+    # ks_statistic, p_value = a_test.statistic, a_test.pvalue
+    # print(f"Waiting Time KS Statistic: {ks_statistic}")
+    # print(f"Waiting Time P-value: {p_value}, {p_value < 0.05}: different distributions")
+    # ax[0, 4].violinplot([WT_mean_synth_sim[map_idx_sim], WT_mean_synth[0]], showmeans=True)
+    # if p_value < 0.05:
+    #     ax[0, 4].set_title(f'Waiting Time\n(Different)')
+    # else:
+    #     ax[0, 4].set_title(f'Waiting Time\n(Same)')
+    # ax[0, 4].set_ylabel(f'Waiting Time (sec)\nMean per Cell')
+    # ax[0, 4].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[1, 4].violinplot([WT_mean_synth_sim_averg, WT_mean_synth_averg], showmeans=True)
+    # ax[1, 4].set_ylabel(f'Waiting Time (sec)\nPopulation Mean')
+    # ax[1, 4].set_xticks([1, 2], ['Simulation', 'Synthetic'])
+    # ax[2, 4].violinplot([WT_mean_synth_sim[map_idx_sim]] + [WT_mean_synth_sim[i] for i in range(n_sim_plots)],
+    #                     showmeans=True)
+    # ax[2, 4].set_xticks(np.arange(n_sim_plots + 1) + 1, ['Map'] + ['Simulation'] * n_sim_plots, rotation=60)
+    # plt.savefig(f'{checkpoint_path}/Summary Stats.png')
+    # plt.show()
